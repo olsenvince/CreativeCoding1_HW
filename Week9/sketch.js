d0680f7e5f04f9afc26f9752c1cc91387570555d
Vince <156894363+olsenvince@users.noreply.github.com>
2024-03-31 16:08:40 -0600

    Create sketch.js

---
+++ Week9/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(300, 400);
+}
+
+function draw() {
+  background(220);
+  rect(38,45,224,265)
+  ellipse(35,175,30,50)
+  ellipse(265,175,30,50)
+  triangle(-30,400,150,310,330,400)
+  rect(105,270,90,95)
+  ellipse(150,170,200,230)
+  ellipse(105,160,40,30)
+  ellipse(190,160,40,30)
+  line(80,144,130,144)
+  line(220,144,165,144)
+  point(150,230)
+  rect(30,190,10,15)
+  rect(260,190,10,15)
+  text('"silly portrait"',115,20)
+  text('Vince Olsen',115,390)
+}
